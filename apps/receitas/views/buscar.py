187ffe0f1c4c1237ed20e453f88a26b97b909a80
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-01-17 13:25:23 -0300

    refatoracao

--- apps/receitas/views/buscar.py
+++ apps/receitas/views/buscar.py
@@ -6,8 +6,7 @@ def buscar(request):
 
     if 'buscar' in request.GET:
         nome_a_buscar = request.GET['buscar']
-        if buscar:
-            lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar) 
+        lista_receitas = lista_receitas.filter(nome_receita__icontains=nome_a_buscar) 
 
     dados = {
         'receitas' : lista_receitas
